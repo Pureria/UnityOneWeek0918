4268ee8f0c47f8040be78182c385487370de755a
siroemon <113779763+siroemon@users.noreply.github.com>
2023-09-19 18:14:20 +0900

    ShutterDoorのスクリプト

---
+++ Assets/_Script/Object/ShutterDoor/ObjectShutterDoor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace MorseGame.Object
+{
+    public class ObjectShutterDoor : ObjectBase
+    {
+        [SerializeField, Tooltip("ゲーム開始時にドアが開いているか？TRUE:開いている FALSE:開いていない")]
+        private bool InitShutterDoor = false;
+        [SerializeField]
+        private float ShutterDoorChangeTime;
+
+        [SerializeField]
+        private GameObject ShutterDoor;
+
+        private float ChangeStartTime;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            NowState = InitShutterDoor;
+            if (NowState) ShutterDoor.SetActive(false);
+            else ShutterDoor.SetActive(true);
+        }
+
+        public override void ReceiveInteract()
+        {
+            base.ReceiveInteract();
+
+            ChangeStartTime = Time.time;
+            NowState = !NowState;
+
+            if (NowState) ShutterDoor.SetActive(false);
+            else ShutterDoor.SetActive(true);
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+        }
+    }
+
+
+}
