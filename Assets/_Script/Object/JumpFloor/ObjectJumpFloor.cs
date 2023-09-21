44e0f832b7b71b1c178812cfd47fc51cd75cce3c
siroemon <113779763+siroemon@users.noreply.github.com>
2023-09-21 23:03:27 +0900

    ギミック作りの前準備

---
+++ Assets/_Script/Object/JumpFloor/ObjectJumpFloor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MorseGame.Object
+{
+    public class ObjectJumpFloor : ObjectBase
+    {
+        
+    }
+
+}
